2b3f60a8749b86515a62c37f055f80f4b2764bad
Safarqwerty <safar.alam79@gmail.com>
2024-06-30 12:48:55 +0800

    hilangkan scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',  /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          'display': 'none',  /* Safari and Chrome */
+        },
+      })
+    },
+  ],
 }
 
